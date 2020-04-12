39db08e0c1bf68142d846ded355238ac85340cc8
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2020-04-12 13:49:29 +0530

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -1,19 +1,23 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
-mongoose.connect("mongodb+srv://SINGHHR:hello123@cluster0-4497t.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
-mongoose.set("useFindAndModify", false);
-
 var UserSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  dob: { type: Date, default: Date.now },
-  email: String,
-  bio: String,
-  image: String,
-  phoneNumber: Number
+    username: String,
+    password: String,
+    dob: {type: Date, default: Date.now},
+    email: String,
+    bio: String,
+    coins: {type: Number, default: 0},
+    image: String,
+    phoneNumber: Number,
+    Post: [
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    }
+    ]
 });
 
-UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose)
 
 module.exports = mongoose.model("User", UserSchema);
